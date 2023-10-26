43e1c2c0ea23ff392d6b51f22d1929e5503e83b9
Thiyaghessan <thiyaghessan@gmail.com>
2023-10-26 10:29:44 -0400

    Add documentation for helper functions
    Add roxygen2 documentation to helper functions used to build catalog
    pages in build-catalog-functions.R. The documentation helps develoeprs
    understand what each function does to make debugging easier.

--- catalogs/build-catalog-functions.R
+++ catalogs/build-catalog-functions.R
@@ -1,5 +1,28 @@
 # Script with helper functions to construct catalog pages
 
+
+#' @title Retrieve paths to files in S3 objects
+#' 
+#' @description get_file_paths uses regular expressions to match the keys of
+#' objects hosted on an AWS S3 bucket. A record of all objects in a bucket must
+#' first be constructed
+#' 
+#' @param series character scalar. Name of NCCS data series. Accepted values
+#' are "core", "bmf".
+#' @param paths character vector. Vector of object keys from S3 bucket.
+#' @param tscope character scalar. Type of tax exemption. Accepted values are
+#'               "NONPROFIT" ( All other 501c type organizations besides 501c3), 
+#'               "CHARITIES" (501c3 nonprofit organizations), 
+#'               "PRIVFOUND" (501c3 private foundations). 
+#'               Default == NULL
+#' @param fscope character scalar. Form type. Accepted values are:
+#'               "PZ": 990 + 990EZ filers
+#'               "PC": 990 filers only
+#'               "PF": 990PF private foundations
+#'               Default == NULL     
+#' 
+#' @return character vector. Matching paths
+
 get_file_paths <- function(series,
                            paths,
                            tscope = NULL,
@@ -17,12 +40,14 @@ get_file_paths <- function(series,
   
 }
 
-get_core_paths <- function( paths, tscope="NONPROFIT", fscope="PZ" ) {
-  expr <- paste0( "CORE-[0-9]{4}-501C[0-9A-Z]-", tscope )
-  matches <- grep( expr, paths, value=T )
-  matches <- grep( paste0( "-", fscope, "\\b"), matches, value=T )
-  return( matches )
-}
+#' @title Create download links for objects in S3 buckets
+#' 
+#' @description make_s3_urls creates hyperlinks that directly point to s3 objects
+#' and trigger a download
+#' 
+#' @param paths character vector. Vector of object keys from S3 bucket.
+#' 
+#' @return character vector. Links to download objects in paths
 
 make_s3_urls <- function( paths ) {
   base <- "https://nccsdata.s3.us-east-1.amazonaws.com/"
@@ -30,6 +55,17 @@ make_s3_urls <- function( paths ) {
   return( urls )
 }
 
+#' @title Create links to archived web pages in https://urbaninstitute.github.io/nccs-legacy/
+#' 
+#' @description make_archive_urls creates hyperlinks that directly point to 
+#' archived web pages from the now defunct NCCS Data Archive. These pages describe
+#' the datasets belonging to an S3 object with a key specified in paths.
+#' 
+#' @param series character scalar. Name of NCCS data series. Accepted values
+#' are "core", "bmf".
+#' @param paths character vector. Vector of object keys from S3 bucket.
+#' 
+#' @return character vector. Links to archived web pages for object keys in paths
 make_archive_urls <- function(series,
                               paths){
   
@@ -48,6 +84,15 @@ make_archive_urls <- function(series,
 }
 
 
+#' @title Extract the tax year for a data set
+#' 
+#' @description get_year uses regular expressions to extract the tax year 
+#' (format: yyyy) for which a data set with a specific object key in 'paths'
+#' covers
+#' 
+#' @param paths character vector. Vector of object keys from S3 bucket.
+#' 
+#' @return character vector. Tax years for datasets.
 
 get_year <- function( paths ) {
   yyyy <- stringr::str_extract( paths, "-[0-9]{4}-" )
@@ -55,6 +100,15 @@ get_year <- function( paths ) {
   return( yyyy )
 }
 
+#' @title Extract the release month for a data set
+#' 
+#' @description get_year uses regular expressions to extract the release month
+#' (format: yyyy) for which a "bmf" data set with a specific object key in 'paths'
+#' covers
+#' 
+#' @param paths character vector. Vector of bmf object keys from S3 bucket.
+#' 
+#' @return character vector. Release months for datasets.
 
 get_month <- function( paths ) {
   mm <- stringr::str_extract( paths, "-[0-9]{2}-" )
@@ -62,6 +116,31 @@ get_month <- function( paths ) {
   return( mm )
 }
 
+#' @title Create html cide for download buttons
+#' 
+#' @description make_buttons creates html code for buttons on a catalog page 
+#' that either link to the download url for an s3 object or the archived data 
+#' dictionary
+#' 
+#' @param urls character vector. Vector of links to s3 object downloads or
+#' archived data dictionaries.
+#' @param button_name character scalar. Name of button to construct. Accepted
+#' values are "DOWNLOAD" or "PROFILE".
+#' 
+#' @return character scalar. HTML code to construct button.
+
+make_buttons <- function(urls, button_name) {
+  
+  button_dic = list("download" = " class='button'> DOWNLOAD </a>",
+                    "profile" = " class='button2'> PROFILE </a>")
+  
+  buttons <- paste0("<a href=",
+                    urls,
+                    button_dic[[button_name]] )
+  return( buttons ) 
+}
+
+##### Undocumented functions #########
 
 
 get_core_tscope <- function( paths ) {
@@ -86,26 +165,6 @@ get_core_fscope <- function( paths ) {
   fscope <- gsub( "^-|-$", "", fscope )
   return( as.character(fscope) )
 }
-
-make_download_buttons <- function( urls ) {
-  buttons <- 
-    paste0( 
-      "<a href=",
-      urls,
-      " class='button'> DOWNLOAD </a>" )
-  return( buttons ) 
-}
-
-make_profile_buttons <- function( urls ) {
-  buttons <- 
-    paste0( 
-      "<a href=",
-      urls,
-      " class='button2'> PROFILE </a>" )
-  return( buttons ) 
-}
-
-
 
 ############
 ############   EFILE
